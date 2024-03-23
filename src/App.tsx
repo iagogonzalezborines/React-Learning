6fdc9d4ded0bfeeea3b4f475f6bf854110115ae0
iagogonzalezborines <119060993+iagogonzalezborines@users.noreply.github.com>
2024-03-23 12:22:57 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
     </div>
   );
 }
